3c025496be767dbf70138ba0aebbdbbf1f2d89c1
Nikita Chauhan <nikitachauhan5499@gmail.com>
2021-09-18 14:17:08 +0530

    Search for links in html

--- src/index.js
+++ src/index.js
@@ -1,11 +1,38 @@
+import cheerio from 'cheerio';
 import fetch from 'node-fetch';
 
+const visited = {};
+const URL = "https://www.amazon.in";
+
+const getValidLink = (link) => {
+    if(link.includes('http'))
+        return link;
+    if (link.startsWith('/'))
+        return `${URL}${link}`;
+    return `${URL}/${link}`;
+}
+
 const fetchHTML = async ({ url }) => {
+    if(visited[url]) return;
+    
+    visited[url] = true;
+
     const response = await fetch(url);
     const html = await response.text();
-    console.log("html: ", html);
+    const $ = cheerio.load(html);
+    
+    const links = $('a').map((i, link) => 
+        link.attribs.href
+    ).get();
+    
+    links.forEach((link) => {
+        fetchHTML({
+            url: getValidLink(link)
+        })
+    })
+    console.log(links);
 }
 
 fetchHTML({
-    url: "https://www.simplesite.com/"
+    url: URL
 })
